0e9c2f6ba81d8f7e2227d4197d6b3f9b4a7fd1e2
armejbha <abdurrahim181878@gmil.com>
2022-02-17 23:23:09 +0600

    all complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// const calculate 
+// input value function 
 function getInputAmount(input) {
     let amountValue = document.getElementById(input + '-input').value;
     if (amountValue == '') {
@@ -7,125 +7,84 @@ function getInputAmount(input) {
     else {
         return amountValue;
     }
-    // if (isNaN(amountValue)) {
-    //     alert('please a valid number');
-    // }
-    // else {
-    // return amountValue;
-    // }
 }
+// cost calculate function 
 function getTotalCost() {
     const foodCost = parseInt(getInputAmount('food'));
     const rentCost = parseInt(getInputAmount('rent'));
     const clothesCost = parseInt(getInputAmount('clothes'));
-    if (foodCost < 0 || rentCost < 0 || clothesCost < 0) {
-        return alert('please enter a positive number');
-    }
-    else {
-        if (isNaN(foodCost) || isNaN(rentCost) || isNaN(clothesCost)) {
-            return alert('please enter a valid number');
+    if (foodCost >= 0 && rentCost >= 0 && clothesCost >= 0) {
+        if (isNaN(foodCost) && isNaN(rentCost) && isNaN(clothesCost)) {
+            return alert('please enter a valid number!');
         }
         else {
             const totalCost = foodCost + rentCost + clothesCost;
             return totalCost;
         }
     }
-    // if (isNaN(foodCost)) {
-    //     alert('please enter a valid food cost number');
-    // }
-    // else if (isNaN(rentCost)) {
-    //     alert('please enter a valid rent cost number');
-    // }
-    // else if (isNaN(clothesCost)) {
-    //     alert('please enter a valid clothes cost number');
-    // }
-    // else {
-    //     const totalCost = foodCost + rentCost + clothesCost;
-    //     return totalCost;
-    // }
-
+    else {
+        return alert('please enter a valid number!');
+    }
 }
-// function getBalance() {
-//     const incomeAmount = getInputAmount('income-input');
-//     if (totalCostAmount() > incomeAmount) {
-//         alert('please decreae your cost');
-//     }
-//     else {
-//         const totalBalance = incomeAmount - totalCostAmount();
-//         return totalBalance;
-//     }
-// }
-// cost calculate button 
+// costing and saving button 
 document.getElementById('cost-button').addEventListener('click', function () {
-    // consting 
-    const cost = getTotalCost();
-    // console.log(cost); 
+    const incomeAmount = getInputAmount('income');
+    const incomeValue = parseInt(incomeAmount);
+    if (isNaN(incomeValue)) {
+        return alert('please enter a valid number!');
+    }
     const totalExpensesAmount = document.getElementById('total-expenses');
-    const oldTotalCostInner = totalExpensesAmount.innerText;
-    // console.log(totalCostInner);
-    totalExpensesAmount.innerText = cost;
     const totalCostInner = totalExpensesAmount.innerText;
-    // incomeAmount 
-    const incomeAmount = parseInt(getInputAmount('income'));
-    if (incomeAmount < 0) {
-        alert('please enter a valid number');
-    }
-    if (incomeAmount < cost) {
-        alert('you spend to much money!');
-    }
-    // balance 
     const balance = document.getElementById('balance');
     const balanceAmount = balance.innerText;
-
-    // console.log(cost);
-    // console.log(totalCostInner);
-    if (totalCostInner != 'undefined') {
+    const cost = getTotalCost();
+    if (incomeValue < cost) {
+        return alert('you spend to much money!');
+    }
+    // error handelin and balance amount calculate 
+    if (cost != undefined) {
         totalExpensesAmount.innerText = cost;
-        if (isNaN(incomeAmount)) {
+        if (isNaN(incomeValue)) {
             balance.innerText = balanceAmount;
         }
         else {
             const totalBalance = incomeAmount - cost;
             balance.innerText = totalBalance;
         }
-        // const totalBalance = incomeAmount - getTotalCost();
-        // balance.innerText = totalBalance;
     }
     else {
-        totalExpensesAmount.innerText = oldTotalCostInner;
+        totalExpensesAmount.innerText = totalCostInner;
         balance.innerText = balanceAmount;
     }
-    // document.getElementById('balance').innerText = getBalance();
 })
-
-// saving calculate 
-
-// saving calculate botton 
+// saving and remainig button 
 document.getElementById('saving-calculate').addEventListener('click', function () {
+    const savingBalance = document.getElementById('saving');
+    const savingInner = savingBalance.innerText;
     const incomeAmount = parseInt(getInputAmount('income'));
     const percent = parseInt(getInputAmount('percent'));
-    if (isNaN(percent)) {
+    if (percent < 0) {
         return alert('please enter a valid number');
     }
-    const savingBalance = document.getElementById('saving');
     const savingAmount = incomeAmount * percent / 100;
-    savingBalance.innerText = savingAmount;
-    const cost = getTotalCost();
-    const balance = incomeAmount - cost;
-    const remainingBalance = document.getElementById('remaining');
-    if (savingAmount > balance) {
-        return alert('you can not save money');
+    if (isNaN(savingAmount)) {
+        return alert('please enter a valid number');
     }
     else {
-        const remainingAmount = balance - savingAmount;
-        remainingBalance.innerText = remainingAmount;
+        if (isNaN(percent)) {
+            return alert('please enter a valid number');
+        }
+        savingBalance.innerText = savingAmount;
+        const cost = getTotalCost();
+        const balance = incomeAmount - cost;
+        // remaining balance calculate 
+        const remainingBalance = document.getElementById('remaining');
+        if (savingAmount > balance) {
+            return alert('you can not save money');
+        }
+        else {
+            const remainingAmount = balance - savingAmount;
+            remainingBalance.innerText = remainingAmount;
+        }
     }
-    // if (getBalance() < savingAmount) {
-    //     alert('Oop! you can not save money');
-    //     document.getElementById('saving').innerText = '';
-    // }
-    // else {
-    //     const remainingAmount = getBalance() - savingAmount;
-    //     document.getElementById('remaining').innerText = parseInt(remainingAmount);
-    // }
 })
